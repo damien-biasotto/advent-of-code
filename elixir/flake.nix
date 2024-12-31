223444a86650ad219261dea4ae0bce18f2c5bb85
Damien Biasotto <damien.biasotto@gmail.com>
2024-12-31 22:38:35 +1100

    Fixing elixir-ls support in a (sort of) monorepo

--- elixir/flake.nix
+++ elixir/flake.nix
@@ -15,13 +15,18 @@
 	buildInputs = with pkgs; [elixir elixir-ls];
 	shellHook =
 	  let
+	    project = pkgs.writeTextFile {
+	      name = ".project";
+	      text = ''
+((nil . ((project-root . default-directory))))
+		'';
+	    };
 	    dir-locals = pkgs.writeTextFile {
 	      name = ".dir-locals.el";
 	      text = ''
 ((elixir-ts-mode . ((eglot-workspace-configuration
                      . (:elixirLS (:dialyzerEnabled :json-false
                                   :enableTestLenses t
-				  :projectDir (locate-dominating-file default-directory "mix.exs")
                                   :fetchDeps t
                                   :mixEnv "dev")))
                     (eglot-server-programs
@@ -29,10 +34,11 @@
                     (eval . (progn
 		    (define-key elixir-ts-mode-map (kbd "C-S-n") #'treesit-beginning-of-defun)
                (define-key elixir-ts-mode-map (kbd "C-S-p") #'treesit-end-of-defun)
-                             (eglot-ensure))))))'';
+                             (eglot-ensure))))))))'';
 	    };
 	  in 
 	    ''
+	      cat ${project} > 2024/.project
 	      cat ${dir-locals} > 2024/.dir-locals.el
 	    '';
       };
